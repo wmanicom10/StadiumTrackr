0a844aef8eb127d07b77f4cce082736183f506e0
Will Manicom <wmanicom11@gmail.com>
2025-03-25 16:42:02 -0400

    undid change

--- scripts/home-script.js
+++ scripts/home-script.js
@@ -97,8 +97,7 @@ logIn.addEventListener('click', async () => {
     const password = passwordInput.value.trim();
 
     try {
-//        const response = await fetch('http://localhost:3000/auth/login', {
-        const response = await fetch('https://stadiumtrackr.com/al', {
+            const response = await fetch('http://localhost:3000/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
