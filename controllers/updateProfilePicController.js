8273155c834729d17121464007116658028013da
cmfisch <carl@carlmfischer.com>
2026-05-15 12:36:35 -0500

    Fix profile pic storage to write directly to EFS path

--- controllers/updateProfilePicController.js
+++ controllers/updateProfilePicController.js
@@ -3,10 +3,11 @@ const path = require('path');
 const db = require('../database/connection.js');
 const { getUserId } = require('../database/dbHelpers.js');
 const fs = require('fs');
+const PROFILE_PIC_DIR = process.env.PROFILE_PIC_DIR;
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'images/profile-pics/');
+        cb(null, PROFILE_PIC_DIR);
     },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
@@ -28,19 +29,21 @@ const handleUpdateProfilePic = async (req, res) => {
 
         const [[user]] = await db.execute('SELECT profile_pic FROM users WHERE user_id = ?', [userId]);
         if (user.profile_pic && !user.profile_pic.includes('default.png')) {
-            if (fs.existsSync(user.profile_pic)) {
-                fs.unlinkSync(user.profile_pic);
+            const fullOldPath = path.join(PROFILE_PIC_DIR, path.basename(user.profile_pic));
+            if (fs.existsSync(fullOldPath)) {
+                fs.unlinkSync(fullOldPath);
             }
         }
 
         const newFilename = `user_${userId}${ext}`;
-        const oldPath = `images/profile-pics/${req.file.filename}`;
-        const newPath = `images/profile-pics/${newFilename}`;
+	const oldPath = path.join(PROFILE_PIC_DIR, req.file.filename);
+	const newPath = path.join(PROFILE_PIC_DIR, newFilename);
+	const webPath = `images/profile-pics/${newFilename}`;
 
         fs.renameSync(oldPath, newPath);
 
-        await db.query('UPDATE users SET profile_pic = ? WHERE user_id = ?', [newPath, userId]);
-        res.json({ profile_pic: newPath });
+        await db.query('UPDATE users SET profile_pic = ? WHERE user_id = ?', [webPath, userId]);
+        res.json({ profile_pic: webPath });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal server error' });
